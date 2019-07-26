b9013b153c74c00329e5256fa69cb3d836a1f2fa
Pete Schwamb <pete@schwamb.net>
2019-07-26 11:58:41 -0500

    Remove quotes to handle empty list

--- Scripts/copy-plugins.sh
+++ Scripts/copy-plugins.sh
@@ -5,16 +5,12 @@
 #
 #  Copyright © 2019 LoopKit Authors. All rights reserved.
 
-#export BUILT_PRODUCTS_DIR=/Users/pete/Library/Developer/Xcode/DerivedData/Loop-gqwyxtsjfgxofmelczgcwptvkcsn/Build/Products/Debug-iphoneos
-#export PLUGINS_FOLDER_PATH=Loop.app/PlugIns
-#export EXPANDED_CODE_SIGN_IDENTITY=asdf
-
 echo "Looking for plugins in $BUILT_PRODUCTS_DIR"
 
 shopt -s nullglob
 
 # Copy device plugins
-for f in "${BUILT_PRODUCTS_DIR}/*.loopplugin"; do
+for f in ${BUILT_PRODUCTS_DIR}/*.loopplugin; do
   plugin=$(basename $f)
   echo Copying device plugin: $plugin to plugins directory in app
   cp -a $f ${BUILT_PRODUCTS_DIR}/${PLUGINS_FOLDER_PATH}
@@ -23,8 +19,8 @@ for f in "${BUILT_PRODUCTS_DIR}/*.loopplugin"; do
     destination=${BUILT_PRODUCTS_DIR}/${PLUGINS_FOLDER_PATH}/${plugin}
     echo "Signing ${plugin} with ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
     /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags ${destination}
-    for framework_path in "${destination}/Frameworks/*.framework"; do
-      framework=$(basename framework_path)
+    for framework_path in ${destination}/Frameworks/*.framework; do
+      framework=$(basename $framework_path)
       echo "Signing $framework for $plugin with $EXPANDED_CODE_SIGN_IDENTITY_NAME"
       /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags ${framework_path}
     done
