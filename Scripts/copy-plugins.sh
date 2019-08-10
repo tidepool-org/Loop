f040fe7abc29b4dab5a9d97e2150a05a292296e0
Pete Schwamb <pete@schwamb.net>
2019-08-10 09:33:07 -0500

    Update copy frameworks script to dereference framework symlinks

--- Scripts/copy-plugins.sh
+++ Scripts/copy-plugins.sh
@@ -20,19 +20,22 @@ for f in "${BUILT_PRODUCTS_DIR}"/*.loopplugin; do
   plugin_as_framework_path="${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${plugin_basename%.*}.framework"
   echo moving "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${plugin_basename}" to "$plugin_as_framework_path"
   mv "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${plugin_basename}" "$plugin_as_framework_path"
-  cp -a "$f/Frameworks/" "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}"
   if [ "$EXPANDED_CODE_SIGN_IDENTITY" != "-" ]; then
     export CODESIGN_ALLOCATE=${DT_TOOLCHAIN_DIR}/usr/bin/codesign_allocate
     echo "Signing ${plugin} with ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
     /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags "$plugin_as_framework_path"
-    echo "Signing plugin's frameworks in "${f}"/Frameworks/*.framework"
-    for framework_path in "${f}"/Frameworks/*.framework; do
-      framework=$(basename "$framework_path")
+  fi
+  for framework_path in "${f}"/Frameworks/*.framework; do
+    framework=$(basename "$framework_path")
+    echo Copying "$framework_path/." to "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${framework}"
+    cp -a "$framework_path/." "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${framework}"
+    plugin_path="$(readlink "$f" || echo "$f")"
+    if [ "$EXPANDED_CODE_SIGN_IDENTITY" != "-" ]; then
       echo "Signing $framework for $plugin with $EXPANDED_CODE_SIGN_IDENTITY_NAME"
       /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --timestamp=none --preserve-metadata=identifier,entitlements,flags "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${framework}"
-    done
-  else
-    echo "Skipping signing of ${plugin}"
-  fi
+    else
+      echo "Skipping signing of ${plugin}"
+    fi
+  done
 done
 
