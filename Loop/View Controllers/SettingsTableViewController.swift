5697afbbc9b1334df2a75ee09814a7b544b5c064
Anna <anna@tidepool.org>
2020-07-20 09:53:27 -0700

    WIP moving insulin model over

--- Loop/View Controllers/SettingsTableViewController.swift
+++ Loop/View Controllers/SettingsTableViewController.swift
@@ -519,7 +519,8 @@ final class SettingsTableViewController: UITableViewController, IdentifiableClas
                 let modelSelectionView = InsulinModelSelection(
                     viewModel: viewModel,
                     glucoseUnit: glucoseUnit,
-                    supportedModelSettings: .init(fiaspModelEnabled: FeatureFlags.fiaspInsulinModelEnabled, walshModelEnabled: FeatureFlags.walshInsulinModelEnabled)
+                    supportedModelSettings: .init(fiaspModelEnabled: FeatureFlags.fiaspInsulinModelEnabled, walshModelEnabled: FeatureFlags.walshInsulinModelEnabled),
+                    appName: "Tidepool Loop" // ANNA TODO: this def shouldn't be hardcoded
                 )
 
                 let hostingController = DismissibleHostingController(rootView: modelSelectionView, onDisappear: {
