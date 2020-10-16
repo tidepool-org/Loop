4e9d8ca78c54ef98298920dcc932405d37ca1a35
Nathaniel Hamming <nate@hmtconsulting.org>
2020-10-16 13:46:51 -0300

    clean up

--- Loop/Managers/Alerts/AlertManager.swift
+++ Loop/Managers/Alerts/AlertManager.swift
@@ -172,7 +172,7 @@ extension AlertManager {
             case .success(let alerts):
                 alerts.forEach { alert in
                     do {
-                        self.replayAlert(try Alert.init(from: alert, adjustedForStorageTime: true))
+                        self.replayAlert(try Alert(from: alert, adjustedForStorageTime: true))
                     } catch {
                         self.log.error("Error decoding alert from persistent storage: %@", error.localizedDescription)
                     }
@@ -186,7 +186,7 @@ extension AlertManager {
             case .success(let alerts):
                 alerts.forEach { alert in
                     do {
-                        self.replayAlert(try Alert.init(from: alert, adjustedForStorageTime: true))
+                        self.replayAlert(try Alert(from: alert, adjustedForStorageTime: true))
                     } catch {
                         self.log.error("Error decoding alert from persistent storage: %@", error.localizedDescription)
                     }
