d254c5584442aeef41d05e4b166e25f6867d1450
Nathaniel Hamming <nate@hmtconsulting.org>
2020-11-19 16:01:34 -0400

    type casting to display the mock pump HUD view

--- Common/Models/PumpManagerUI.swift
+++ Common/Models/PumpManagerUI.swift
@@ -9,6 +9,7 @@
 import Foundation
 import LoopKit
 import LoopKitUI
+import MockKit
 import MockKitUI
 
 private let managersByIdentifier: [String: PumpManagerUI.Type] = staticPumpManagers.compactMap{ $0 as? PumpManagerUI.Type}.reduce(into: [:]) { (map, Type) in
@@ -25,6 +26,12 @@ func PumpManagerHUDViewFromRawValue(_ rawValue: PumpManagerHUDViewRawValue, plug
     {
         return nil
     }
+
+    // this type casting is needed to have the mock pump HUD view display in the widget
+    if let mockPumpManager = manager as? MockPumpManager.Type {
+        return mockPumpManager.createHUDView(rawValue: rawState)
+    }
+
     return manager.createHUDView(rawValue: rawState)
 }
 
