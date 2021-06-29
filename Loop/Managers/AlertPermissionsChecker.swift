4b52478f8723c559c44050c3bc1bf7a592e1949e
Rick Pasetto <rickpasetto@yahoo.com>
2021-06-29 12:27:38 -0700

    PR Feedback

--- Loop/Managers/AlertPermissionsChecker.swift
+++ Loop/Managers/AlertPermissionsChecker.swift
@@ -14,11 +14,14 @@ import SwiftUI
 class AlertPermissionsChecker {
     private static let criticalAlertPermissionsAlertIdentifier = Alert.Identifier(managerIdentifier: "LoopAppManager",
                                                                                   alertIdentifier: "criticalAlertPermissionsAlert")
-    private static let criticalAlertPermissionsAlertContent = Alert.Content(title: NSLocalizedString("Alert Permissions Disabled",
-                                                                                                     comment: "Critical Alert or Notifications permissions disabled alert title"),
-                                                                            body: NSLocalizedString("Keep Notifications and Critical Alerts turned ON in your phone’s settings to ensure that you can receive Tidepool Loop notifications.",
-                                                                                                    comment: "Critical Alert or Notifications permissions disabled alert body"),
-                                                                            acknowledgeActionButtonLabel: NSLocalizedString("OK", comment: "Critical Alert or Notifications permissions disabled alert button"))
+    private static let criticalAlertPermissionsAlertContent = Alert.Content(
+        title: NSLocalizedString("Alert Permissions Disabled",
+                                 comment: "Critical Alert or Notifications permissions disabled alert title"),
+        body: String(format: NSLocalizedString("Keep Notifications and Critical Alerts turned ON in your phone’s settings to ensure that you can receive %1$@ notifications.",
+                                               comment: "Format for critical Alert or Notifications permissions disabled alert body. (1: app name)"),
+                     Bundle.main.bundleDisplayName),
+        acknowledgeActionButtonLabel: NSLocalizedString("OK", comment: "Critical Alert or Notifications permissions disabled alert button")
+    )
     private static let criticalAlertPermissionsAlert = Alert(identifier: criticalAlertPermissionsAlertIdentifier,
                                                              foregroundContent: criticalAlertPermissionsAlertContent,
                                                              backgroundContent: criticalAlertPermissionsAlertContent,
@@ -30,7 +33,7 @@ class AlertPermissionsChecker {
         return UIApplication.shared.applicationState == UIApplication.State.background
     }
     
-    lazy private var cancellables = Set<AnyCancellable>()
+    private lazy var cancellables = Set<AnyCancellable>()
 
     init(alertManager: AlertManager) {
         self.alertManager = alertManager
@@ -94,7 +97,7 @@ class AlertPermissionsChecker {
 extension UserDefaults {
     
     private enum Key: String {
-        case hasUserBeenNotifiedOfPermissionsAlert = "com.loopkit.Loop.AlertPermissionsChecker"
+        case hasUserBeenNotifiedOfPermissionsAlert = "com.loopkit.Loop.HasUserBeenNotifiedOfPermissionsAlert"
     }
     
     var hasUserBeenNotifiedOfPermissionsAlert: Bool {
