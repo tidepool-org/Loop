4395ff35c41238e66a19bb29877caea1fa9efa64
Cameron Ingham <camji555@gmail.com>
2024-12-03 12:12:29 -0800

    [LOOP-5153] Remove HealthKit dependency from LoopAlgorithm

--- Common/Extensions/HKUnit.swift
+++ Common/Extensions/HKUnit.swift
@@ -22,20 +22,6 @@ extension LoopUnit {
         }
     }
 
-    var localizedShortUnitString: String {
-        if self == LoopUnit.millimolesPerLiter {
-            return NSLocalizedString("mmol/L", comment: "The short unit display string for millimoles of glucose per liter")
-        } else if self == .milligramsPerDeciliter {
-            return NSLocalizedString("mg/dL", comment: "The short unit display string for milligrams of glucose per decilter")
-        } else if self == .internationalUnit {
-            return NSLocalizedString("U", comment: "The short unit display string for international units of insulin")
-        } else if self == .gram {
-            return NSLocalizedString("g", comment: "The short unit display string for grams")
-        } else {
-            return String(describing: self)
-        }
-    }
-
     /// The smallest value expected to be visible on a chart
     var chartableIncrement: Double {
         if self == .milligramsPerDeciliter {
