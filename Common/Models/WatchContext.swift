22b7a3a4ab8d1bb63a3cc9eeffda2f881fee632c
Cameron Ingham <camji555@gmail.com>
2025-04-03 13:17:14 -0700

    [LOOP-5280] Display Glucose Preference by InternationalUnit

--- Common/Models/WatchContext.swift
+++ Common/Models/WatchContext.swift
@@ -124,8 +124,7 @@ final class WatchContext: RawRepresentable {
 
         raw["gc"] = glucoseCondition?.rawValue
         raw["gt"] = glucoseTrend?.rawValue
-        if let glucoseTrendRate = glucoseTrendRate {
-            let unitPerMinute = unit.glucose(per: .minutes)
+        if let glucoseTrendRate = glucoseTrendRate, let unitPerMinute = unit.unitDivided(by: .minute) {
             raw["gtru"] = unitPerMinute.unitString
             raw["gtrv"] = glucoseTrendRate.doubleValue(for: unitPerMinute)
         }
