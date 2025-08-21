505cd6049aa1d512cf85c380aeb9542a3f1a47bc
Cameron Ingham <camji555@gmail.com>
2025-08-21 14:02:42 -0700

    [LOOP-5405] Activity Presets Core

--- Loop/Views/Presets/Components/PresetCard.swift
+++ Loop/Views/Presets/Components/PresetCard.swift
@@ -30,7 +30,7 @@ struct PresetCard: View {
 
     var presetTitle: some View {
         HStack(spacing: 6) {
-            if let icon, icon.isEmpty, !icon.isEmpty {
+            if let icon, !icon.isEmpty {
                 PresetSymbolView(icon)
             }
 
