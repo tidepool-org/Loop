f446b028fd73253e084257c9efe12e731b9b1534
Cameron Ingham <camji555@gmail.com>
2025-08-26 11:29:42 -0700

    [LOOP-5405] Add Unmodified Badge to PresetDetentView

--- Loop/Views/Presets/Components/PresetDetentView.swift
+++ Loop/Views/Presets/Components/PresetDetentView.swift
@@ -148,6 +148,16 @@ struct PresetDetentView: View {
                     isActive: temporaryPresetsManager.activePreset?.id == preset.id
                 )
                 
+                if case let .activity(activityPreset) = preset, !activityPreset.isModifiedFromDefault {
+                    Group {
+                        Text(Image(systemName: "checkmark.seal.fill")) + Text(" ") + Text("Recommended starting values")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(Color.accentColor)
+                    .frame(maxWidth: .infinity)
+                    .padding(.bottom, 4)
+                }
+                
                 actionArea
             }
             .toolbar(.hidden)
