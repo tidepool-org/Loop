cdb643e90300d8a4c50c06f89f82c08448171b85
Cameron Ingham <camji555@gmail.com>
2026-07-08 09:12:29 -0700

    [LOOP-5958] Toolbar Orientation Fix (#938)

--- Loop/Views/StatusTableView.swift
+++ Loop/Views/StatusTableView.swift
@@ -262,34 +262,38 @@ struct LegacyTabBarBackground: ViewModifier {
 
 struct ActionTabView<Content: View>: View {
     
-    @State private var orientation: UIDeviceOrientation
+    @State private var orientation: UIInterfaceOrientation
 
-    private let allowedOrientations: [UIDeviceOrientation]
     private let content: Content
     private let tabs: [ActionTab]
     
     init(
-        allowedOrientations: [UIDeviceOrientation] = [.portrait],
         @ViewBuilder content: @escaping () -> Content,
         @ActionTabBuilder tabs: @escaping () -> [ActionTab],
     ) {
         self.content = content()
         self.tabs = tabs()
         
-        self.allowedOrientations = allowedOrientations
-        self.orientation = UIDevice.current.orientation
+        self.orientation = Self.currentOrientation()
     }
     
     var body: some View {
         content
             .safeAreaInset(edge: .bottom, spacing: 0) {
-                if orientation == .unknown || allowedOrientations.contains(orientation) {
+                if orientation.isPortrait {
                     ActionTabBar(items: tabs)
                         .modifier(LegacyTabBarBackground())
                 }
             }
             .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                orientation = UIDevice.current.orientation
+                orientation = Self.currentOrientation()
             }
+    }
+
+    private static func currentOrientation() -> UIInterfaceOrientation {
+        UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .first?
+            .interfaceOrientation ?? .portrait
     }
 }
